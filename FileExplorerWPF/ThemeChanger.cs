c5156c6c68457526bb5ab5b8c8c6c3ee15f0659c
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-11-28 14:06:52 +0100

    Changed themeChanger

--- FileExplorerWPF/ThemeChanger.cs
+++ FileExplorerWPF/ThemeChanger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 
 namespace FileExplorerWPF
 {
@@ -7,29 +8,34 @@ namespace FileExplorerWPF
     /// </summary>
     public static class ThemeChanger
     {
-        public static void SetDarkTheme(MainWindow window)
+        public static void Change(MainWindow window, Theme theme)
         {
             window.Resources.MergedDictionaries.Clear();
-            System.Windows.ResourceDictionary rd = System.Windows.Application.LoadComponent(new Uri("Themes/DarkTheme.xaml", UriKind.Relative)) as System.Windows.ResourceDictionary;
-            window.Resources.MergedDictionaries.Add(rd);
+            window.Resources.MergedDictionaries.Add(getThemeUri(theme));
         }
-        public static void SetLightTheme(MainWindow window)
+        private static ResourceDictionary getThemeUri(Theme theme)
         {
-            window.Resources.MergedDictionaries.Clear();
-            System.Windows.ResourceDictionary rd = System.Windows.Application.LoadComponent(new Uri("Themes/LightTheme.xaml", UriKind.Relative)) as System.Windows.ResourceDictionary;
-            window.Resources.MergedDictionaries.Add(rd);
+            string uri = "";
+            switch (theme)
+            {
+                case Theme.Dark:
+                    uri = "Themes/DarkTheme.xaml";
+                    break;
+                case Theme.Light:
+                    uri = "Themes/LightTheme.xaml";
+                    break;
+                case Theme.ColourfulDark:
+                    uri = "Themes/ColourfulDarkTheme.xaml";
+                    break;
+                case Theme.ColourfulLight:
+                    uri = "Themes/ColourfulLightTheme.xaml";
+                    break;
+            }
+            return Application.LoadComponent(new Uri(uri, UriKind.Relative)) as ResourceDictionary;
         }
-        public static void SetColourfulDarkTheme(MainWindow window)
-        {
-            window.Resources.MergedDictionaries.Clear();
-            System.Windows.ResourceDictionary rd = System.Windows.Application.LoadComponent(new Uri("Themes/ColourfulDarkTheme.xaml", UriKind.Relative)) as System.Windows.ResourceDictionary;
-            window.Resources.MergedDictionaries.Add(rd);
-        }
-        public static void SetColourfulLightTheme(MainWindow window)
-        {
-            window.Resources.MergedDictionaries.Clear();
-            System.Windows.ResourceDictionary rd = System.Windows.Application.LoadComponent(new Uri("Themes/ColourfulLightTheme.xaml", UriKind.Relative)) as System.Windows.ResourceDictionary;
-            window.Resources.MergedDictionaries.Add(rd);
-        }
+    }
+    public enum Theme
+    {
+        Dark, Light, ColourfulDark, ColourfulLight,
     }
 }
