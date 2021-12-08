fced6e3928c5a398fd04b2cb3be3fa4a197dceaa
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-08 16:16:56 +0100

    Visual git account issue

--- FileExplorerWPF/Util/Converters/DateTimeConverter.cs
+++ FileExplorerWPF/Util/Converters/DateTimeConverter.cs
@@ -9,9 +9,7 @@ namespace FileExplorerWPF.Util.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if(value is DateTime date)
-            {
                 return String.Format("{0:d/M/yyyy HH:mm:ss}", date);
-            }
             return null;
         }
 
