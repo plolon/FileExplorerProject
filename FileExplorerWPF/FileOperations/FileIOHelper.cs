70b6248eb05a5142ea20ee67d3cc8412215c2079
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-13 16:37:34 +0100

    Added static class to file io

---
+++ FileExplorerWPF/FileOperations/FileIOHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace FileExplorerWPF.FileOperations
+{
+    public static class FileIOHelper
+    {
+        public static void Move(this string file, string destination, bool cut)
+        {
+            //move file
+        }
+        public static void Remove(this string file)
+        {
+            //remvoe file
+        }
+
+        public static string Create(string destination)
+        {
+            //create new file
+            return "";
+        }
+    }
+}
