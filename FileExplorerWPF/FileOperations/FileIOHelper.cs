222c628a01d539e6abfb4d370b297c2ea7f73568
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-14 10:03:49 +0100

    Created basic handler for file move

--- FileExplorerWPF/FileOperations/FileIOHelper.cs
+++ FileExplorerWPF/FileOperations/FileIOHelper.cs
@@ -1,13 +1,22 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace FileExplorerWPF.FileOperations
 {
     public static class FileIOHelper
     {
-        public static void Move(this string file, string destination, bool cut)
+        public static void Move(this string file, string destination, MoveType type)
         {
-            //move file
+
         }
+        public static void Move(this List<string> files, string destination, MoveType type)
+        {
+            foreach(var file in files)
+            {
+
+            }
+        }
+
         public static void Remove(this string file)
         {
             //remvoe file
@@ -17,6 +26,21 @@ namespace FileExplorerWPF.FileOperations
         {
             //create new file
             return "";
+        }
+
+        private static void Move(string file, string destination)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Copy(string file, string destination)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Cut(string file, string destination)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
