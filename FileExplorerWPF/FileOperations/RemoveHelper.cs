4e201234d0296e3ebb22680aeb5532b6698448d4
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-15 12:14:34 +0100

    Created current listbox selected checker

---
+++ FileExplorerWPF/FileOperations/RemoveHelper.cs
@@ -0,0 +1,25 @@
+﻿using FileExplorerWPF.Utils;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace FileExplorerWPF.FileOperations
+{
+    public static class RemoveHelper
+    {
+        public static void RemoveFile(this string file)
+        {
+            try
+            {
+                if (file.IsFile())
+                    File.Delete(file);
+                else if (file.IsDirectory())
+                    Directory.Delete(file);
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show($"{e.Message}",$"{e.GetType()}");
+            }
+        }
+    }
+}
