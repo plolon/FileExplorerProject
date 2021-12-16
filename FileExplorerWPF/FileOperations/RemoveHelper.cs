eb8ed7d1801f944abfd27ce39e9e6dca13cf301f
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-16 09:39:40 +0100

    And included into removing directories too

--- FileExplorerWPF/FileOperations/RemoveHelper.cs
+++ FileExplorerWPF/FileOperations/RemoveHelper.cs
@@ -17,40 +17,50 @@ namespace FileExplorerWPF.FileOperations
 
             foreach (string file in files)
             {
-                if (file.IsFile())
+                if (!acceptAll)
                 {
-                    FileInfo fileInfo = new FileInfo(file);
-                    FileModel fileModel = new FileModel()
+                    if (file.IsFile())
                     {
-                        Icon = IconHelper.GetIconOfFile(file, false, false),
-                        Name = fileInfo.Name,
-                        Path = fileInfo.FullName,
-                        DateModified = fileInfo.LastWriteTime,
-                        SizeBytes = fileInfo.Length,
-                    };
-                    if (!acceptAll)
-                    {
-                        if(ShowDialog(fileModel, out acceptAll, "Are you sure you want to permamently delete this file?"))
+                        FileInfo fileInfo = new FileInfo(file);
+                        FileModel fileModel = new FileModel()
+                        {
+                            Icon = IconHelper.GetIconOfFile(file, false, false),
+                            Name = fileInfo.Name,
+                            Path = fileInfo.FullName,
+                            DateModified = fileInfo.LastWriteTime,
+                            SizeBytes = fileInfo.Length,
+                        };
+                        if (ShowDialog(fileModel, out acceptAll, "Are you sure you want to permamently delete this file?"))
                             File.Delete(file);
                     }
-                    else
-                        File.Delete(file);
-                }
-                else if (file.IsDirectory())
-                {
-                    try
+                    else if (file.IsDirectory())
                     {
-                        Directory.Delete(file);
-                    }
-                    catch (IOException e)
-                    {
-                        MessageBoxResult result = MessageBox.Show($"Remove: {file.GetFileName()}, with everything inside?", $"{e.Message}", MessageBoxButton.YesNo);
-                        if (result == MessageBoxResult.Yes)
+                        try
                         {
-                            Directory.Delete(file, true);
+                            Directory.Delete(file);
                         }
-                        else { } //do nothing
+                        catch (IOException e)
+                        {
+                            DirectoryInfo dInfo = new DirectoryInfo(file);
+                            FileModel dModel = new FileModel()
+                            {
+                                Icon = IconHelper.GetIconOfFile(file, false, true),
+                                Name = dInfo.Name,
+                                Path = dInfo.FullName,
+                                DateModified = dInfo.LastWriteTime,
+                                SizeBytes = null
+                            };
+                            if (ShowDialog(dModel, out acceptAll, "Are you sure you want to permamently delete this directory?"))
+                                Directory.Delete(file, true);
+                        }
                     }
+                }
+                else
+                {
+                    if (file.IsFile())
+                        File.Delete(file);
+                    else if (file.IsDirectory())
+                        Directory.Delete(file, true);
                 }
             }
         }
