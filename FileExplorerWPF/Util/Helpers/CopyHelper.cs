be27284296be80029a2292280f6eaa9b5738eb2f
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-12-17 17:37:38 +0100

    Created copyTo Helper with clipboard

---
+++ FileExplorerWPF/Util/Helpers/CopyHelper.cs
@@ -0,0 +1,42 @@
+﻿using FileExplorerWPF.FileOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorerWPF.Util.Helpers
+{
+    public static class CopyHelper
+    {
+        public static bool HasClipBoard = false;
+
+        private static List<string> clipBoard;
+        public static List<string> ClipBoard
+        {
+            get
+            {
+                var buffer = clipBoard;
+                HasClipBoard = false;
+                clipBoard.Clear();
+                return buffer;
+            }
+            set
+            {
+                HasClipBoard = true;
+                clipBoard = value;
+            }
+        }
+
+        public static void CopyTo(string path)
+        {
+            if (HasClipBoard)
+            {
+                foreach (var file in clipBoard)
+                {
+                    file.Move(path);
+                }
+            }
+        }
+    }
+}
